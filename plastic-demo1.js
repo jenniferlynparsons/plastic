a34ce69b2e01b2383c641cff69b66eedcc13ed9f
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2016-10-04 17:40:59 -0400

    Incorporated JS-Standard standards

--- plastic-demo1.js
+++ plastic-demo1.js
@@ -1,65 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* global Plastic, Inventory, _p, getInputVal, checkRadio, printThing, next, chosenThing */
+document.addEventListener('DOMContentLoaded', function () {
+  var plastic = new Plastic({})
+  var state = {}
 
-  var plastic = new Plastic({});
-  var state = {};
-
-  function Character(plastic, charProperties){
-    Character.call(this, plastic, charProperties);
+  function Character (plastic, charProperties) {
+    Character.call(this, plastic, charProperties)
   }
 
-  function Items(plastic, itemProperties){
-    Inventory.call(this, plastic, itemProperties);
+  function Items (plastic, itemProperties) {
+    Inventory.call(this, plastic, itemProperties)
   }
 
-  Character.prototype = Object.create(Character.prototype);
+  Character.prototype = Object.create(Character.prototype)
   Items.prototype = Object.create(Inventory.prototype)
 
-
-  function getStarted(plastic){
+  function getStarted (plastic) {
     // TODO: this goes into an inifinte loop
-    // plastic.character = new Character(plastic);
+    // plastic.character = new Character(plastic)
 
-
-    plastic.chosenName = getInputVal(_p("#newName"));
+    plastic.chosenName = getInputVal(_p('#newName'))
 
     // TODO: this doesn't work because _p returns an array
-    state.chosenQuest = checkRadio(_p("#questpicker1 input"));
+    state.chosenQuest = checkRadio(_p('#questpicker1 input'))
 
     // TODO: these don't work because _p returns an array
-    _p('.charname').forEach(function(element){
-      printThing(plastic.chosenName, element);
-    });
+    _p('.charname').forEach(function (element) {
+      printThing(plastic.chosenName, element)
+    })
 
-    _p('.questname').forEach(function(element){
-      printThing(plastic.chosenQuest, element);
-    });
+    _p('.questname').forEach(function (element) {
+      printThing(plastic.chosenQuest, element)
+    })
 
+    plastic.itemList = ['thing one', 'thing two', 'thing three']
 
-
-    plastic.itemList = ['thing one', 'thing two', 'thing three'];
-
-    state.items = new Items(plastic, {name: "Inventory", min: 0, max: 10, autorenew: false, itemList:plastic.itemList, addSuccess:"It added", addFail: "It didn't add", removeSuccess: "It removed", removeFail: "It didn't remove"});
+    state.items = new Items(plastic, {name: 'Inventory', min: 0, max: 10, autorenew: false, itemList: plastic.itemList, addSuccess: 'It added', addFail: 'It didn\t add', removeSuccess: 'It removed', removeFail: 'It didn\'t remove'})
 
     // TODO: fix this
-    // plastic.printInventory(state.items.itemList, _p('#itemListWrapper'));
+    // plastic.printInventory(state.items.itemList, _p('#itemListWrapper'))
 
-    next("#newChar","#start");
+    next('#newChar', '#start')
   }
 
-  function step3(plastic){
-    plastic.things = document.getElementsByName('newQuestItem');
-    plastic.chosenThing = checkRadio(plastic.things);
-    state.items.addItem(chosenThing);
-    state.items.printInventory();
+  function step3 (plastic) {
+    plastic.things = document.getElementsByName('newQuestItem')
+    plastic.chosenThing = checkRadio(plastic.things)
+    state.items.addItem(chosenThing)
+    state.items.printInventory()
   }
 
-  _p('#newChar').addEventListener("click", function(e) {
-    getStarted(plastic);
-  });
+  _p('#newChar').addEventListener('click', function (e) {
+    getStarted(plastic)
+  })
 
-  _p('#addQuestItem').addEventListener("click", function(e) {
-    step3(plastic);
-  });
-
-
-});
+  _p('#addQuestItem').addEventListener('click', function (e) {
+    step3(plastic)
+  })
+})
--- plastic.js
+++ plastic.js
@@ -1,86 +1,85 @@
+/* eslint-disable no-unused-vars */
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 // General plastic functions
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
 // blargh. i don't quite know why i need this right now but i do. once i understand it better, it will all be fixed.
-function Plastic(){
+function Plastic () {
 
 }
 
 // Polyfill for .includes https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
+  String.prototype.includes = function (search, start) {
+    'use strict'
     if (typeof start !== 'number') {
-      start = 0;
+      start = 0
     }
 
     if (start + search.length > this.length) {
-      return false;
+      return false
     } else {
-      return this.indexOf(search, start) !== -1;
+      return this.indexOf(search, start) !== -1
     }
-  };
+  }
 }
 
 // http://libux.co/useful-function-making-queryselectorall-like-jquery/
 // need to update. if the query is for an ID, use querySelector() and return element itself, if the query is for a class, use querySelectorAll() and always return an array, even if there's only one item in it.
 
-function _p(selector, context) {
-  context = context || document;
-  var elementsArray;
-  if(selector.includes('#')){
-    elementsArray = Array.prototype.slice.call(context.querySelectorAll(selector));
-    return elementsArray[0];
-  }else if(selector.includes('.')){
-    elementsArray = Array.prototype.slice.call(context.querySelectorAll(selector));
-    return elementsArray;
+function _p (selector, context) {
+  context = context || document
+  var elementsArray
+  if (selector.includes('#')) {
+    elementsArray = Array.prototype.slice.call(context.querySelectorAll(selector))
+    return elementsArray[0]
+  } else if (selector.includes('.')) {
+    elementsArray = Array.prototype.slice.call(context.querySelectorAll(selector))
+    return elementsArray
   }
 }
 
 // thanks Sal
-function mergeDefaults(obj, defaults, propList){
-  Object.assign(obj, defaults);
-  Object.assign(obj, propList);
+function mergeDefaults (obj, defaults, propList) {
+  Object.assign(obj, defaults)
+  Object.assign(obj, propList)
 }
-
 
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 // UI functions
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
 // On click of element, the element with ID matching href is shown, the current visible element is hidden.
-// This can be used two ways. Typically, it would be used to navigate between "pages" of the game,
+// This can be used two ways. Typically, it would be used to navigate between 'pages' of the game,
 // but it could also be used to hide/reveal bits of information or interactions on the same page.
-function next(anchor, currentVisible){
-  var current = _p(currentVisible);
-  current.classList.add('hidden');
-  var destination = _p(anchor).getAttribute("href").replace(/[^a-zA-Z 0-9]+/g,"");
-  _p(next).classList.remove('hidden');
-  e.preventDefault();
+function next (anchor, currentVisible) {
+  var current = _p(currentVisible)
+  current.classList.add('hidden')
+  var destination = _p(anchor).getAttribute('href').replace(/[^a-zA-Z 0-9]+/g, '')
+  _p(next).classList.remove('hidden')
+  e.preventDefault()
 }
 
 // To output a string to an HTML container
-function printThing(info, wrapper){
-  if(wrapper.length > 1){
-    wrapper.forEach(function(element){
-      element.innerHTML = info;
-    });
-  }else{
-    wrapper.innerHTML = info;
+function printThing (info, wrapper) {
+  if (wrapper.length > 1) {
+    wrapper.forEach(function (element) {
+      element.innerHTML = info
+    })
+  } else {
+    wrapper.innerHTML = info
   }
 }
 
 // This will always output a <ul> with inner <li>s and insert them into the wrapper element
-function printInventory(plastic, wrapper){
-  var printItems;
+function printInventory (plastic, wrapper) {
+  var printItems
   for (var i = 0, length = plastic.items.length; i < length; ++i) {
-    printItems += "<li>" + items[i] + "</li>";
+    printItems += '<li>' + plastic.items[i] + '</li>'
   }
-  var printList = "<ul>" + printItems + "</ul";
-  wrapper.innerHTML(printList);
+  var printList = '<ul>' + printItems + '</ul>'
+  wrapper.innerHTML(printList)
 }
-
 
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 // form element functions
@@ -89,58 +88,57 @@ function printInventory(plastic, wrapper){
 // TODO: the form elements should be processed on click of submit
 
 // TODO: could be handled with an array of elements?
-function getInputVal(inputId){
-  return inputId.value;
+function getInputVal (inputId) {
+  return inputId.value
 }
 
-function checkRadio(radioArray){
+function checkRadio (radioArray) {
   for (var i = 0, length = radioArray.length; i < length; i++) {
     if (radioArray[i].checked) {
-      return radioArray[i].value;
+      return radioArray[i].value
     }
   }
 }
-
 
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 // Character functions
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
-function Character(plastic, propList){
-  var defaults = {name:"Character", currentQuest: "none"};
-  mergeDefaults(this, defaults, propList);
-};
-
+function Character (plastic, propList) {
+  var defaults = {name: 'Character', currentQuest: 'none'}
+  mergeDefaults(this, defaults, propList)
+}
 
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 // inventory functions
 // *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
-function Inventory(propList){
-  var defaults = {name: "Default Inventory", min: 0, max: 100, updateSuccess: "Item Added", updateFail: "Inventory Full", removeSuccess: "Item Removed", removeFail: "Inventory Empty"};
-  mergeDefaults(this, defaults, propList);
+function Inventory (propList) {
+  var defaults = {name: 'Default Inventory', min: 0, max: 100, updateSuccess: 'Item Added', updateFail: 'Inventory Full', removeSuccess: 'Item Removed', removeFail: 'Inventory Empty'}
+  mergeDefaults(this, defaults, propList)
 }
 
-Inventory.prototype.addItem = function(plastic, newItem){
-  if(plastic.itemList.length < plastic.max){
-    plastic.itemList.push(newItem);
-    return plastic.updateSuccess;
-  }else{
-    return plastic.updateFail;
+Inventory.prototype.addItem = function (plastic, newItem) {
+  if (plastic.itemList.length < plastic.max) {
+    plastic.itemList.push(newItem)
+    return plastic.updateSuccess
+  } else {
+    return plastic.updateFail
   }
 }
 
-Inventory.prototype.subtractItem = function(removeItem){
-  var shorterArray;
-  for(var i = 0, listLength = plastic.itemList.length; i <= listLength; i++){
-    if(plastic.itemList[i] === removeItem){
-      shorterArray = plastic.itemlist.splice(plastic.itemlist[i], 1);
+Inventory.prototype.subtractItem = function (plastic, removeItem) {
+  var shorterArray
+  for (var i = 0, listLength = plastic.itemList.length; i <= listLength; i++) {
+    if (plastic.itemList[i] === removeItem) {
+      shorterArray = plastic.itemlist.splice(plastic.itemlist[i], 1)
     }
   }
-  if(shorterArray.length > plastic.min){
-    plastic.itemList = plastic.itemList.remove(removeItem);
-    return plastic.removeSuccess;
-  }else{
-    return plastic.removeFail;
+  if (shorterArray.length > plastic.min) {
+    plastic.itemList = plastic.itemList.remove(removeItem)
+    return plastic.removeSuccess
+  } else {
+    return plastic.removeFail
   }
 }
+/* eslint-enable no-unused-vars */
