249d00f03d953523b61195727c7eca8eca3d4eef
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2016-11-02 08:26:17 -0400

    adding function and data outlines

---
+++ design_documents/possible-functions.js
@@ -0,0 +1,24 @@
+// inventories
+viewInventory() // return list of items in inventory
+emptyInventory() // remove everything from the inventory
+addToInventory()
+removeFromInventory()
+itemIsInInventory()
+
+// items
+viewItemProps()
+createItem()
+destroyItem()
+itemIsEquippable()
+equipItem()
+
+// activities
+startInteraction()
+endInteraction()
+useInventoryItem()
+
+// character stats
+// stats can be modified by inventory items and by interactions
+statActive()
+statValue()
+modifyValue()
