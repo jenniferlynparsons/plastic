bbec82a4d33f770dfc202d2250f3ed056ca84f54
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2016-12-09 10:04:27 -0500

    update with more comments

--- demo1/player-0.1.0.js
+++ demo1/player-0.1.0.js
@@ -4,6 +4,9 @@ var player = new Character({
   "level": 1
 });
 
+// HI CECELIA! this is the call that's not working properly. 
+// when this is called, player is created and has an empty inventory array.
+// not sure if the NewInventoryItem function is needed or if it would be better to just call new InventoryItem
 player.addInventoryItem(
   new NewInventoryItem("gold", 10)
 );
